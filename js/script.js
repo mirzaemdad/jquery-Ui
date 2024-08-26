678474e99e3a26ec414c450e286695fe0a499fae
mirzaemdad <mirzaemdad4@gmail.com>
2024-08-26 19:22:21 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$( function() {
+    $( "#draggable" ).draggable();
+
+
+    $(".box").draggable();
+
+    $( "#draggable2" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+
+    $( "#resizable" ).resizable();
+
+    $("#selectable").selectable();
+
+    $("#sortable").sortable();
+
+    $("#accordion").accordion();
+
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+
+    $( "#progressbar" ).progressbar({
+      value: 60
+    });
+
+    $( "#slider" ).slider();
+
+    var state = true;
+    $( "#button" ).on( "click", function() {
+      if ( state ) {
+        $( "#effect" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+    });
+
+  } );
